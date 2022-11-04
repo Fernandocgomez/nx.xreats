13ef7e920089ed88dedfb43cd81adc8d56b786fb
fernandocgomez <fernandocgomez@live.com>
2022-11-04 00:32:14 -0500

    fixed typo on folder

--- libs/ui/forms/src/lib/ui-forms.module.ts
+++ libs/ui/forms/src/lib/ui-forms.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { InputTextControlComponent } from './form-controllers/components/input-text-control/input-text-control.component';
+import { InputTextControlComponent } from './form-controls/components/input-text-control/input-text-control.component';
 
 @NgModule({
   imports: [CommonModule],
