958eb226afeaf8b9be44636473201ea04885a25b
fernandocgomez <fernandocgomez@live.com>
2022-11-03 22:49:47 -0500

    Initial commit

---
+++ apps/xr-menu/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'xreats-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'xr-menu';
+}
